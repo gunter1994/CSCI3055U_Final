dc3274c6efb2c03c6fc0320296815e9375dbfb7e
gunter1994 <hunter.thompson@uoit.net>
2016-11-29 15:21:48 -0500

    Added test folder for learning swift

---
+++ test/hello.swift
@@ -0,0 +1,9 @@
+print ("Hello World")
+
+var mutable = 6
+print ("vars can change, mutable: " + String(mutable))
+mutable = 8
+print ("mutable: " + String(mutable))
+let constant = "something"
+print ("using let makes a constant, constant: " + constant) 
+//constant = "something else" makes an error
