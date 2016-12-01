3cfbf892fb4ebb9f027eb7b371db8f036a0646b6
gunter1994 <hunter.thompson@uoit.net>
2016-11-30 19:18:50 -0500

    Implimented basic versions of guice and ashley plugins

--- test/core/src/com/mygdx/game/MyGdxGame.kt
+++ test/core/src/com/mygdx/game/MyGdxGame.kt
@@ -1,23 +1,34 @@
 package com.mygdx.game
 
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.google.inject.Binder
+import com.google.inject.Guice
+import com.google.inject.Injector
+import com.google.inject.Module
 
 class MyGdxGame : ApplicationAdapter() {
     internal lateinit var batch: SpriteBatch
     internal lateinit var img: Texture
+    internal val engine = Engine()
+    private lateinit var injector: Injector
 
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
+        injector  = Guice.createInjector(GameModule(this))
+        engine.addSystem(SpamSystem())
     }
 
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        engine.update(Gdx.graphics.deltaTime)
         batch.begin()
         batch.draw(img, 0f, 0f)
         batch.end()
@@ -26,5 +37,17 @@ class MyGdxGame : ApplicationAdapter() {
     override fun dispose() {
         batch.dispose()
         img.dispose()
+    }
+}
+
+class SpamSystem : EntitySystem() {
+    override fun update(deltaTime: Float) {
+        println(deltaTime)
+    }
+}
+
+class GameModule(private val myGdxGame: MyGdxGame) : Module {
+    override fun configure(binder: Binder) {
+        binder.bind(SpriteBatch::class.java).toInstance(myGdxGame.batch)
     }
 }
