1887960464e96ad4c2a768d5f4d496944bcb49bd
gunter1994 <hunter.thompson@uoit.net>
2016-12-08 22:22:02 -0500

    Added a lot of comments, making functions clearer and cleaned up code a bit

--- test/core/src/com/mygdx/game/MyGdxGame.kt
+++ test/core/src/com/mygdx/game/MyGdxGame.kt
@@ -11,23 +11,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.google.inject.*
 
+//most included methods at this point is from youtube series.
 class MyGdxGame : ApplicationAdapter() {
     internal lateinit var batch: SpriteBatch
     internal lateinit var img: Texture
     internal val engine = Engine()
     private lateinit var injector: Injector
 
+    //creates instance of object given a texture.
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
+        //injects the object into the game system
         injector  = Guice.createInjector(GameModule(this))
         injector.getInstance(Systems::class.java).list.map { injector.getInstance(it) }.forEach { system ->
             engine.addSystem(system)
         }
-
         createEntities()
     }
 
+    //adds entity to game engine, sets up it' texture and transform components
     private fun createEntities() {
         engine.addEntity(Entity().apply {
             add(TextureComponent(img))
@@ -35,32 +38,38 @@ class MyGdxGame : ApplicationAdapter() {
         })
     }
 
+    //renters the game graphics
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         engine.update(Gdx.graphics.deltaTime)
     }
 
+    //delets graphics
     override fun dispose() {
         batch.dispose()
         img.dispose()
     }
 }
 
+//used for testing objects and features, just prints a lot of info very fast
 //class SpamSystem @Inject constructor(private val spriteBatch: SpriteBatch) : EntitySystem() {
 //    override fun update(deltaTime: Float) {
 //        println(deltaTime.toString() + "; " + spriteBatch)
 //    }
 //}
+
+//renders current view using camera and sprites able to be seen
 class RenderingSystem @Inject constructor(private val batch: SpriteBatch,
                                           private val camera: OrthographicCamera) : IteratingSystem(Family.all(TransformComponent::class.java, TextureComponent::class.java).get()) {
     override fun update(deltaTime: Float) {
-        batch.projectionMatrix = camera.combined
+        batch.projectionMatrix = camera.combined //loads the cameras current matrix
         batch.begin()
         super.update(deltaTime)
         batch.end()
     }
 
+    //process's an entity and draws it's current location
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val img = entity.texture.texture
         val position = entity.transform.position
@@ -68,14 +77,17 @@ class RenderingSystem @Inject constructor(private val batch: SpriteBatch,
     }
 }
 
+//usfull for position tracking with the camera
 val Int.pixelsToMeters: Float
         get() = this / 32F
 
+//module for game, sets up different features such as camera and systems
 class GameModule(private val myGdxGame: MyGdxGame) : Module {
     override fun configure(binder: Binder) {
         binder.bind(SpriteBatch::class.java).toInstance(myGdxGame.batch)
     }
 
+    //sets up the rendering system
     @Provides @Singleton
     fun systems() : Systems {
         return Systems(listOf(
@@ -83,9 +95,10 @@ class GameModule(private val myGdxGame: MyGdxGame) : Module {
         ))
     }
 
+    //camera used to display objects in the sim
     @Provides @Singleton
     fun camera() : OrthographicCamera {
-        val viewportWidth = Gdx.graphics.width.pixelsToMeters
+        val viewportWidth = Gdx.graphics.width.pixelsToMeters //makes it default to see entire scene at start
         val viewportHeight = Gdx.graphics.height.pixelsToMeters
         return OrthographicCamera(viewportWidth, viewportHeight).apply {
             position.set(viewportWidth / 2F, viewportHeight / 2F, 0F)
